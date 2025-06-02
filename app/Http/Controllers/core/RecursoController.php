671a582daf08b8d97b349984cc61dbbaf43d0dff
Pedro Barr <pebarreraa@udistrital.edu.co>
2025-06-02 09:47:01 -0500

    Corregir funcionalidad creación de entidades recurso en controlador recursos

--- app/Http/Controllers/core/RecursoController.php
+++ app/Http/Controllers/core/RecursoController.php
@@ -36,26 +36,77 @@ class RecursoController extends Controller
      */
     public function store(Request $request)
     {
-      $espc_descripcin = $request->input('espc_descripcion');
-      $tp_rec_diminutivo = $request->input('tp_rec_diminutivo');
+      try {
+        $datos = $request->all();
+        $contenido = $datos;
+      } catch (\Exception $e) {
+        $contenido = [];
+      }
+
+      $espc_descripcin = (
+          $contenido["especificacion"] ??
+          $contenido["espc_descripcion"]
+        ) ??
+        $request->input('espc_descripcion')
+      ;
+
+      if ($request->has('tp_rec_diminutivo') || isset($contenido["tp_rec_diminutivo"])) {
+        $tp_rec_diminutivo = $request->input('tp_rec_diminutivo');
+      }
+
+      if (isset($contenido["tp_rec_id"]) || $request->has('tipo_recurso')) {
+        $tp_rec_id = $contenido["tipo_recurso"] ?? $contenido["tp_rec_id"];
+      }
+
       $tp_perm_nombre = 'accesible';
 
-      $rec_nombre = $request->input('rec_nombre');
-      $rec_descripcion = $request->input('rec_descripcion');
-      $rec_diminutivo = $request->input('rec_diminutivo');
+      $rec_nombre = (
+          $contenido["nombre"] ??
+          $contenido["rec_nombre"]
+        ) ??
+        $request->input('rec_nombre')
+      ;
+
+      $rec_descripcion = (
+          $contenido["descripcion"] ??
+          $contenido["rec_descripcion"]
+        ) ??
+        $request->input('rec_descripcion')
+      ;
+      
+      if ($request->has('rec_diminutivo') || isset($contenido["rec_diminutivo"])) {
+        $rec_diminutivo = $request->input('rec_diminutivo');
+      } else {
+        $rec_diminutivo = strtolower(
+          preg_replace('/[^a-z0-9]+/i', '_', $rec_nombre)
+        );
+
+        $rec_diminutivo .= '_' . time();
+      }
 
       $archivo_controller = new ArchivoController();
       $response = $archivo_controller->store($request);
+
+      if ($response->status() == 400) {
+        return $response;
+      }
+      
       $archivo = $response->original;
 
       $especificacion_recurso = EspecificacionRecurso::create([
         "espc_descripcin" => $espc_descripcin,
       ]);
 
-      $tipo_recurso = TipoRecurso
-        ::where('tp_rec_diminutivo', $tp_rec_diminutivo)
-        ->first()
-      ;
+      if (isset($tp_rec_id)) {
+        $tipo_recurso = TipoRecurso::findOrFail($tp_rec_id);
+      } else if (isset($tp_rec_diminutivo)) {
+        $tipo_recurso = TipoRecurso
+          ::where('tp_rec_diminutivo', $tp_rec_diminutivo)
+          ->first()
+        ;
+      } else {
+        $tipo_recurso = TipoRecurso::first();
+      }
 
       $tipo_permiso = TipoPermiso
         ::where('tp_perm_nombre', $tp_perm_nombre)
@@ -72,17 +123,21 @@ class RecursoController extends Controller
         "arch_id" => $archivo["id"],
       ]);
 
-      $respuesta = Response::json([
-          "rec_nombre" => $recurso->rec_nombre,
-          "rec_diminutivo" => $recurso->rec_diminutivo,
-          "espc_descripcion" => $especificacion_recurso->espc_descripcin,
-          "tp_rec_nombre" => $tipo_recurso->tp_rec_nombre,
-          "tp_perm_nombre" => $tipo_permiso->tp_perm_nombre,
-          "arch_nombre" => $archivo["nombre"],
-          "arch_uri" => $archivo["uri"],
-      ]);
+      if (count($contenido) == 0) {
+        $respuesta = Response::json([
+            "rec_nombre" => $recurso->rec_nombre,
+            "rec_diminutivo" => $recurso->rec_diminutivo,
+            "espc_descripcion" => $especificacion_recurso->espc_descripcin,
+            "tp_rec_nombre" => $tipo_recurso->tp_rec_nombre,
+            "tp_perm_nombre" => $tipo_permiso->tp_perm_nombre,
+            "arch_nombre" => $archivo["nombre"],
+            "arch_uri" => $archivo["uri"],
+        ]);
 
-      return $respuesta;
+        return $respuesta;
+      } else {
+        return $recurso;
+      }
     }
 
     /**
